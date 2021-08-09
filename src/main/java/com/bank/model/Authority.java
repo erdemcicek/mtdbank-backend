fb73640d2e9568392a9f06c3e560a8bd02b030ef
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-08-09 23:32:17 +0200

    Update Authority.java

--- src/main/java/com/bank/model/Authority.java
+++ src/main/java/com/bank/model/Authority.java
@@ -7,11 +7,16 @@ public class Authority implements GrantedAuthority {
 	private static final long serialVersionUID = 1L;
 	private final String authority;
 	
+	
     public Authority(String authority) {
         this.authority = authority;
     }
+	
+	
     @Override
     public String getAuthority() {
         return authority;
     }
+	
+	
 }
